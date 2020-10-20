9b44f8200caddf3b983b910a2f5d00627aff1973
domitalk <dominic.m.chu@gmail.com>
2020-10-19 21:52:26 -0400

    seperated responsive container + menu as a sep component

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -15,6 +15,7 @@ import {
   Sidebar,
   Visibility,
 } from 'semantic-ui-react'
+import ResponsiveContainer from '../components/ResponsiveContainer'
 
 const { MediaContextProvider, Media } = createMedia({
   breakpoints: {
@@ -28,188 +29,188 @@ const { MediaContextProvider, Media } = createMedia({
  * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
  * components for such things.
  */
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as='h1'
-      content='Find your issues'
-      inverted
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Put your money where your heart is'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
-    </Button>
-  </Container>
-)
+// const HomepageHeading = ({ mobile }) => (
+//   <Container text>
+//     <Header
+//       as='h1'
+//       content='Find your issues'
+//       inverted
+//       style={{
+//         fontSize: mobile ? '2em' : '4em',
+//         fontWeight: 'normal',
+//         marginBottom: 0,
+//         marginTop: mobile ? '1.5em' : '3em',
+//       }}
+//     />
+//     <Header
+//       as='h2'
+//       content='Put your money where your heart is'
+//       inverted
+//       style={{
+//         fontSize: mobile ? '1.5em' : '1.7em',
+//         fontWeight: 'normal',
+//         marginTop: mobile ? '0.5em' : '1.5em',
+//       }}
+//     />
+//     <Button primary size='huge'>
+//       Get Started
+//       <Icon name='right arrow' />
+//     </Button>
+//   </Container>
+// )
 
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
+// HomepageHeading.propTypes = {
+//   mobile: PropTypes.bool,
+// }
 
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-class DesktopContainer extends Component {
-  state = {}
+// /* Heads up!
+//  * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
+//  * It can be more complicated, but you can create really flexible markup.
+//  */
+// class DesktopContainer extends Component {
+//   state = {}
 
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
+//   hideFixedMenu = () => this.setState({ fixed: false })
+//   showFixedMenu = () => this.setState({ fixed: true })
 
-  render() {
-    const { children } = this.props
-    const { fixed } = this.state
+//   render() {
+//     const { children } = this.props
+//     const { fixed } = this.state
 
-    return (
-      <Media greaterThan='mobile'>
-        <Visibility
-          once={false}
-          onBottomPassed={this.showFixedMenu}
-          onBottomPassedReverse={this.hideFixedMenu}
-        >
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
-            vertical
-          >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              <Container>
-                <Menu.Item as='a' active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as='a'>Issues</Menu.Item>
-                <Menu.Item as='a'>Elections</Menu.Item>
-                <Menu.Item as='a'>Get Involved</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>
-                    Log in
-                  </Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
-          </Segment>
-        </Visibility>
+//     return (
+//       <Media greaterThan='mobile'>
+//         <Visibility
+//           once={false}
+//           onBottomPassed={this.showFixedMenu}
+//           onBottomPassedReverse={this.hideFixedMenu}
+//         >
+//           <Segment
+//             inverted
+//             textAlign='center'
+//             style={{ minHeight: 700, padding: '1em 0em' }}
+//             vertical
+//           >
+//             <Menu
+//               fixed={fixed ? 'top' : null}
+//               inverted={!fixed}
+//               pointing={!fixed}
+//               secondary={!fixed}
+//               size='large'
+//             >
+//               <Container>
+//                 <Menu.Item as='a' active>
+//                   Home
+//                 </Menu.Item>
+//                 <Menu.Item as='a'>Issues</Menu.Item>
+//                 <Menu.Item as='a'>Elections</Menu.Item>
+//                 <Menu.Item as='a'>Get Involved</Menu.Item>
+//                 <Menu.Item position='right'>
+//                   <Button as='a' inverted={!fixed}>
+//                     Log in
+//                   </Button>
+//                   <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+//                     Sign Up
+//                   </Button>
+//                 </Menu.Item>
+//               </Container>
+//             </Menu>
+//             <HomepageHeading />
+//           </Segment>
+//         </Visibility>
 
-        {children}
-      </Media>
-    )
-  }
-}
+//         {children}
+//       </Media>
+//     )
+//   }
+// }
 
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
+// DesktopContainer.propTypes = {
+//   children: PropTypes.node,
+// }
 
-class MobileContainer extends Component {
-  state = {}
+// class MobileContainer extends Component {
+//   state = {}
 
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+//   handleSidebarHide = () => this.setState({ sidebarOpened: false })
 
-  handleToggle = () => this.setState({ sidebarOpened: true })
+//   handleToggle = () => this.setState({ sidebarOpened: true })
 
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
+//   render() {
+//     const { children } = this.props
+//     const { sidebarOpened } = this.state
 
-    return (
-      <Media as={Sidebar.Pushable} at='mobile'>
-        <Sidebar.Pushable>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={sidebarOpened}
-          >
-            <Menu.Item as='a' active>
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>Issues</Menu.Item>
-            <Menu.Item as='a'>Elections</Menu.Item>
-            <Menu.Item as='a'>Get Involved</Menu.Item>
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item>
-          </Sidebar>
+//     return (
+//       <Media as={Sidebar.Pushable} at='mobile'>
+//         <Sidebar.Pushable>
+//           <Sidebar
+//             as={Menu}
+//             animation='overlay'
+//             inverted
+//             onHide={this.handleSidebarHide}
+//             vertical
+//             visible={sidebarOpened}
+//           >
+//             <Menu.Item as='a' active>
+//               Home
+//             </Menu.Item>
+//             <Menu.Item as='a'>Issues</Menu.Item>
+//             <Menu.Item as='a'>Elections</Menu.Item>
+//             <Menu.Item as='a'>Get Involved</Menu.Item>
+//             <Menu.Item as='a'>Log in</Menu.Item>
+//             <Menu.Item as='a'>Sign Up</Menu.Item>
+//           </Sidebar>
 
-          <Sidebar.Pusher dimmed={sidebarOpened}>
-            <Segment
-              inverted
-              textAlign='center'
-              style={{ minHeight: 350, padding: '1em 0em' }}
-              vertical
-            >
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Button as='a' inverted>
-                      Log in
-                    </Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                      Sign Up
-                    </Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile />
-            </Segment>
+//           <Sidebar.Pusher dimmed={sidebarOpened}>
+//             <Segment
+//               inverted
+//               textAlign='center'
+//               style={{ minHeight: 350, padding: '1em 0em' }}
+//               vertical
+//             >
+//               <Container>
+//                 <Menu inverted pointing secondary size='large'>
+//                   <Menu.Item onClick={this.handleToggle}>
+//                     <Icon name='sidebar' />
+//                   </Menu.Item>
+//                   <Menu.Item position='right'>
+//                     <Button as='a' inverted>
+//                       Log in
+//                     </Button>
+//                     <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+//                       Sign Up
+//                     </Button>
+//                   </Menu.Item>
+//                 </Menu>
+//               </Container>
+//               <HomepageHeading mobile />
+//             </Segment>
 
-            {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Media>
-    )
-  }
-}
+//             {children}
+//           </Sidebar.Pusher>
+//         </Sidebar.Pushable>
+//       </Media>
+//     )
+//   }
+// }
 
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
+// MobileContainer.propTypes = {
+//   children: PropTypes.node,
+// }
 
-const ResponsiveContainer = ({ children }) => (
-  /* Heads up!
-   * For large applications it may not be best option to put all page into these containers at
-   * they will be rendered twice for SSR.
-   */
-  <MediaContextProvider>
-    <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </MediaContextProvider>
-)
+// const ResponsiveContainer = ({ children }) => (
+//   /* Heads up!
+//    * For large applications it may not be best option to put all page into these containers at
+//    * they will be rendered twice for SSR.
+//    */
+//   <MediaContextProvider>
+//     <DesktopContainer>{children}</DesktopContainer>
+//     <MobileContainer>{children}</MobileContainer>
+//   </MediaContextProvider>
+// )
 
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
+// ResponsiveContainer.propTypes = {
+//   children: PropTypes.node,
+// }
 
 const HomepageLayout = () => (
   <ResponsiveContainer>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,1 +1,10 @@
 import React, { Component } from 'react'
+import ResponsiveContainer from '../components/ResponsiveContainer'
+
+const SearchPage = () => {
+    <div> 
+        
+    </div>
+}; 
+
+export default SearchPage;
