0a7dd799d0255c27c0b02244a4bb93e27b5b8b95
domitalk <dominic.m.chu@gmail.com>
2020-10-19 21:55:05 -0400

    search page basic responsivee wrapper added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomepageLayout from './pages/Homepage';
+// need to bring in react-router into here and make it so you can change views 
+// also bring in reduxpthunk here 
 
 function App() {
   return (
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react'
 import ResponsiveContainer from '../components/ResponsiveContainer'
 
 const SearchPage = () => {
-    <div> 
-        
-    </div>
+    <ResponsiveContainer> 
+        <div>
+
+        </div>
+    </ResponsiveContainer>
 }; 
 
 export default SearchPage;
